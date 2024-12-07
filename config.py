4eef47bdf0624d294d58afd4608f41e11e416378
MyungJune Shin <kevinshin123@gmail.com>
2024-12-07 16:53:05 +0900

    Work more on reproducibility. Training from scratch is reproduced. Loading a model and training more epochs from there isn't reproduced, reason unknown.

--- config.py
+++ config.py
@@ -1,13 +1,21 @@
+import datetime
+current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
 # File Path #
-log_file = './stdout.log'
+log_file = "./stdout.log"
 train_file = "./yelp2018/train.txt"
 test_file = "./yelp2018/test.txt"
 adj_mat_file = "./yelp2018/s_pre_adj_mat.npz"
-metric_result_file = './result/metric.tsv'
-best_model_dir = './result/best_model'
+metric_result_file = f"./result/{current_time}_metric.tsv"
+best_model_dir = f"./result/{current_time}_best_model"
+
+load_model = False
+if load_model:
+    model_file = "./best_model_epoch1.pth"
 
 # Hyperparams #
-epochs = 100
+start_epoch = 1  # should be 1 typically, and not 1 if you want to continue training
+epochs = 1000  # meaning the end epoch, so `epochs - start_epoch + 1` is the number of epochs to train
 n_layers = 3
 embed_dim = 64
 train_batch_size = 2048
@@ -16,11 +24,16 @@ do_neg_sampling = True
 reg_strength = 1e-4
 
 # Loss #
-loss_fn = 'bpr'
+loss_fn = "bpr"
 
 # Eval #
 topk = 20
-metrics = ['recall', 'ndcg', 'diversity']
+metrics = ["recall", "ndcg", "diversity"]
 
 # Random Seed #
 seed = 2024
+
+# Sanity Check #
+assert start_epoch <= epochs, "start_epoch should be less than or equal to epochs"
+assert (load_model == True and start_epoch > 1) or (load_model == False and start_epoch == 1), "start_epoch should be 1 if load_model is False"
+assert (loss_fn == "bpr" and do_neg_sampling == True) or (loss_fn != "bpr"), "BPR loss requires negative sampling"
--- misc.py
+++ misc.py
@@ -1,9 +1,28 @@
 import numpy as np
 import torch
+import random
+import os
 
 def set_seed(seed):
+    """
+    reference: https://pytorch.org/docs/2.5/notes/randomness.html#reproducibility
+    """
+    random.seed(seed)
     np.random.seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.benchmark = False
+        os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
     torch.manual_seed(seed)
+    torch.use_deterministic_algorithms(True)
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
+
+def get_generator(seed):
+    generator = torch.Generator()
+    generator.manual_seed(seed)
+    return generator
